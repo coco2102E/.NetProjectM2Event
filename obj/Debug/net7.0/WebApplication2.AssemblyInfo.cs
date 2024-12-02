b9370c481c80a11d538d4be78c5df26c3f939f9d
Ravelomanantsoa Iaina Nomena Erico <95394975+ericonomena@users.noreply.github.com>
2024-12-02 18:33:56 +0300

    push
    farany

--- obj/Debug/net7.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net7.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e2a471c83669dd2d87187bc0c0c80f0539bbb7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+670e3ca8f3272b40e1c8f78338f1b62f4a06dd31")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
